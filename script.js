fdbee927cadd6283ba370f82cc2863d2968b0707
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-02-14 09:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+    var select = document.getElementById("colorSelect");
+        if (select.selectedIndex !== -1) {
+        // Remove the selected option
+        select.remove(select.selectedIndex);
+    }
+}
