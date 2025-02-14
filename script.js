f234592e5b9e66a8ea1bc647e8ec1362e4145d81
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-02-14 12:56:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
 function removeColor() {
     var select = document.getElementById("colorSelect");
-        if (select.selectedIndex !== -1) {
-        // Remove the selected option
-        select.remove(select.selectedIndex);
+        if (select.index !== -1) {
+        select.remove(select.index);
     }
 }
+
+
